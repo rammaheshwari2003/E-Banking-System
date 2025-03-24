11a1c022866ed26b2746aa767a90d2fbda79b6ef
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2025-03-24 14:25:59 +0530

    Delete Client directory

--- Client/src/main.jsx
+++
@@ -1,7 +0,0 @@
-
-import App from "./App";
-import { createRoot } from "react-dom/client";
-import'bootstrap/dist/css/bootstrap.min.css';
-import "./css/style.css"
-
-createRoot(document.getElementById("root")).render(<App />);
